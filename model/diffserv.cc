b176126d6060023a8db3316da7ca481b2da7b9da
JinglinZhou99 <jzhou66@dons.usfca.edu>
2025-05-03 17:06:16 -0700

    add methods in diffserv files

--- model/diffserv.cc
+++ model/diffserv.cc
@@ -2,41 +2,80 @@
 
 namespace ns3 {
 
-NS_OBJECT_ENSURE_REGISTERED(DiffServ);
+DiffServ::DiffServ() {}
 
-TypeId DiffServ::GetTypeId(void) {
-    static TypeId tid = TypeId("ns3::DiffServ")
-        .SetParent<Queue<Packet>>()
-        .SetGroupName("Network");
-    return tid;
-}
-
-DiffServ::DiffServ() {
-}
-
-DiffServ::~DiffServ() {
-    q_class.clear();
+bool DiffServ::Enqueue(Ptr<Packet> p){
+    return DoEnqueue(p);
 }
 
 bool DiffServ::DoEnqueue(Ptr<Packet> p) {
-    uint32_t queueIndex = Classify(p);
-    if (queueIndex >= q_class.size()) {
+    uint32_t queue_index = Classify(p);
+    if (queue_index >= q_class.size()) {
         return false;
     }
+    return q_class[queue_index]->Enqueue(p);
+}
 
-    return q_class[queueIndex]->Enqueue(p);
+Ptr<Packet> DiffServ::Dequeue() {
+    return DoDequeue();
 }
 
 Ptr<Packet> DiffServ::DoDequeue() {
-    return Schedule();
+    Ptr<const Packet> dpacket = Schedule();
+    if (dpacket)
+    {
+        uint32_t index = Classify(dpacket->Copy());
+        return q_class[index]->Dequeue();
+    }
+    return nullptr;
+}
+
+Ptr<Packet> DiffServ::Remove() {
+    return DoRemove();
 }
 
 Ptr<Packet> DiffServ::DoRemove() {
+    Ptr<const Packet> rpacket = Schedule();
+    if (rpacket != nullptr)
+    {
+        uint32_t index = Classify(rpacket->Copy());
+        return q_class[index]->Remove();
+    }
     return nullptr;
 }
 
-Ptr<Packet> DiffServ::DoPeek() const {
-    return nullptr;
+Ptr<const Packet> DiffServ::Peek() const {
+    return DoPeek();
 }
 
+Ptr<const Packet> DiffServ::DoPeek() const {
+    Ptr<const Packet> next_packet = Schedule()->Copy();
+    return next_packet;
+}
+
+uint32_t DiffServ::Classify(Ptr<Packet> p) {
+    uint32_t index = -1;
+    for (int i = 0; i < q_class.size(); i++)
+    {
+        if (q_class[i]->match(p))
+        {
+            return i;
+        }
+        if (q_class[i]->GetDefault())
+        {
+            index = i;
+        }
+    }
+    return index;
+}
+
+void DiffServ::AddQueue(TrafficClass* trafficClass)
+{
+    q_class.push_back(trafficClass);
+}
+
+std::vector<TrafficClass*> DiffServ::GetQueues() const
+{
+    return q_class;
+}
 } // namespace ns3
