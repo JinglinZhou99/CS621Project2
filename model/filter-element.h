62991d3753c9144d8ed915906946e79d0433a09c
JinglinZhou99 <jzhou66@dons.usfca.edu>
2025-05-03 20:23:54 -0700

    Add classes in filter element files

--- model/filter-element.h
+++ model/filter-element.h
@@ -3,18 +3,98 @@
 
 #include "ns3/packet.h"
 #include "ns3/object.h"
+#include "ns3/ppp-header.h"
+#include "ns3/ipv4-header.h"
+#include "ns3/udp-header.h"
+#include "ns3/tcp-header.h"
 
 namespace ns3 {
 
-class FilterElement : public Object {
+class FilterElement {
 public:
-    static TypeId GetTypeId(void);
-
-    FilterElement();
-    virtual ~FilterElement();
-
     // Pure virtual method to check if the packet matches the condition
-    virtual bool Match(Ptr<Packet> p) const = 0;
+    virtual bool match(Ptr<Packet> p) const = 0;
+};
+
+class SrcIPAddress : public FilterElement {
+private:
+    Ipv4Address default_address;
+public:
+    SrcIPAddress(Ipv4Address addr);
+    bool match(Ptr<Packet> p) const override;
+}
+
+/** SrcMask subclass */
+class SrcMask : public FilterElement {
+private:
+    Ipv4Address default_address;
+    Ipv4Mask default_mask;
+
+public:
+    SrcMask(Ipv4Address addr, Ipv4Mask mask);
+
+    bool match(Ptr<Packet> p) const override;
+};
+
+/** SrcPortNumber subclass */
+class SrcPortNumber : public FilterElement
+{
+    private:
+        uint32_t default_port;
+
+    public:
+        SrcPortNumber(uint32_t port);
+
+        bool match(Ptr<Packet> p) const override;
+};
+
+/** DstIPAddress subclass */
+class DstIPAddress : public FilterElement
+{
+    private:
+        Ipv4Address default_address;
+
+    public:
+        DstIPAddress(Ipv4Address addr);
+
+        bool match(Ptr<Packet> p) const override;
+};
+
+/** DstMask subclass */
+class DstMask : public FilterElement
+{
+    private:
+        Ipv4Address default_address;
+        Ipv4Mask default_mask;
+
+    public:
+        DstMask(Ipv4Address addr, Ipv4Mask mask);
+
+        bool match(Ptr<Packet> p) const override;
+};
+
+/** DstPortNumber subclass */
+class DstPortNumber : public FilterElement
+{
+    private:
+        uint32_t default_port;
+
+    public:
+        DstPortNumber(uint32_t port);
+
+        bool match(Ptr<Packet> p) const override;
+};
+
+/** ProtocolNumber subclass */
+class ProtocolNumber : public FilterElement
+{
+    private:
+        uint32_t default_protocol;
+
+    public:
+        ProtocolNumber(uint32_t protocol);
+
+        bool match(Ptr<Packet> p) const override;
 };
 
 } // namespace ns3
